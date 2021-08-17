b417fa575b9a7ba2c8efdaa6c6289c8f5e459157
dami <damixyz@outlook.com>
2021-08-17 22:56:46 +0100

    display a line of text

---
+++ lesson2/fig02_01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Welcome to c++! 🚀\n"; // display message
+    return 0;                          // indicate that program ended successfully
+} // end function main
